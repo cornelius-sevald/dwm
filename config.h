81288f107be26d423ec3b2b071f21163c4a3f08b
Cornelius Sevald-Krause <cornelius.sevald@protonmail.com>
2021-09-14 19:32:20 +0200

    Add rule for raylib apps
    This is nice when developing games with raylib.

--- config.h
+++ config.h
@@ -32,6 +32,7 @@ static const Rule rules[] = {
 	{ "firefox",     NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "discord",     NULL,     NULL,           1,         0,          0,          -1,         1 },
 	{ "st-256color", NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "raylib",      NULL,     NULL,           0,         1,          0,          -1,        -1 },
 	{ NULL,          NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
